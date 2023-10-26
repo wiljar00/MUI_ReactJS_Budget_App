84ffc0d9c73a8bc593492aa8a927d319abfb4fce
Jarom Wilcox <wiljar00@users.noreply.github.com>
2023-10-25 22:54:09 -0600

    CSS Changes
    Add a cool animation to the total Expenses value.

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,19 +1,35 @@
-import React from 'react';
+// src/components/IncomeExpense.js
+import React, { useEffect, useState, useRef } from 'react';
 
 const IncomeExpense = ({ expenses }) => {
-    // Function to calculate the total sum of all expenses
-    const totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
-  
-    return (
-      <div className="income-expense-container">
-        <div className="item">
-          <h2>Income: $XXXX</h2> {/* Replace XXXX with the actual income value */}
-        </div>
-        <div className="item">
-          <h2>Expenses: ${totalExpenses.toFixed(2)}</h2> 
-        </div>
-      </div>
+  const [totalExpenses, setTotalExpenses] = useState(0);
+  const totalExpensesRef = useRef(null);
+
+  useEffect(() => {
+    setTotalExpenses(
+      expenses.reduce((total, expense) => total + expense.amount, 0)
     );
-  };
+    if (totalExpensesRef.current) {
+      totalExpensesRef.current.classList.remove("animate");
+      void totalExpensesRef.current.offsetWidth; // Trigger reflow to restart the animation
+      totalExpensesRef.current.classList.add("animate");
+    }
+  }, [expenses]);
+
+  return (
+    <div className="income-expense-container">
+      <div className="item">
+        <h2 style={{ animation: "fadeInAnimation 2s ease-in-out" }}>
+          Income: $XXXX
+        </h2>
+      </div>
+      <div className="item">
+        <h2>
+          Expenses: $<span ref={totalExpensesRef} className="animate">{totalExpenses.toFixed(2)}</span>
+        </h2>
+      </div>
+    </div>
+  );
+};
 
 export default IncomeExpense;
