b43509c231854a0b36f8598839557f4687a6f5bf
Jarom Wilcox <wiljar00@users.noreply.github.com>
2023-10-31 23:53:59 -0600

    Update Homepage
    Add background image to homepage and additional css effects.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,22 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Homepage = () => {
-  return (
-    <div className="homepage-container">
-      <h1>Welcome to Your Financial Tracker</h1>
-      <p className="app-description">
-        Your one-stop solution for managing your finances efficiently and effortlessly.
-      </p>
-      <div className="button-container">
-        <Link to="/graphs">
-          <button className="homepage-button">Go to Graphs</button>
-        </Link>
-        <Link to="/budget">
-          <button className="homepage-button">Go to Budget</button>
-        </Link>
+    return (
+      <div className="homepage-container">
+        <div className="background-image">
+          <div className="background-overlay"></div>
+          <div className="content">
+            <h1>Welcome to Your Financial Tracker</h1>
+            <p className="app-description">
+              Your one-stop solution for managing your finances efficiently and effortlessly.
+            </p>
+            <div className="button-container">
+              <Link to="/graphs">
+                <button className="homepage-button">Go to Graphs</button>
+              </Link>
+              <Link to="/budget">
+                <button className="homepage-button">Go to Budget</button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default Homepage;
